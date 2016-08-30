a252756a597682730b90b22a5e9290d27b0083b6
hrgdavor <hrgdavor@gmail.com>
2016-08-30 09:29:14 +0200

    fix hip_map that broke during refactoring

--- src/hipster_map.php
+++ src/hipster_map.php
@@ -15,11 +15,17 @@ namespace {
 		
 		$map = array();
 		
-		if(is_array($rows)) foreach($rows as $row){
-			if($column)
-				hip_map_assoc_fill($map,$row,$column,0);
-			else
-				hip_map_fill($map,$row, 0);
+		if(is_array($rows) && count($rows)){
+			if($column){
+				foreach($rows as $row){
+					hip_map_assoc_fill($map,$row,$column,0);
+				}
+			}else{
+				$column = array_keys($rows[0]);
+				foreach($rows as $row){
+					hip_map_fill($map,$row, $column, 0);
+				}
+			}
 		}
 
 		return $map;
@@ -30,12 +36,18 @@ namespace {
 		
 		$map = array();
 		
-		if(is_array($rows)) foreach($rows as $row){
-			if($column)
-				hip_map_assoc_list_fill($map,$row,$column, 0);
-			else
-				hip_map_list_fill($map,$row, 0);
-		}
+		if(is_array($rows) && count($rows)){
+			if($column){
+				foreach($rows as $row){
+					hip_map_assoc_list_fill($map,$row,$column, 0);
+				}
+			}else{
+				$column = array_keys($rows[0]);
+				foreach($rows as $row){
+					hip_map_list_fill($map,$row, $column, 0);
+				}
+			}
+		} 
 		
 		return $map;
 	}
@@ -45,23 +57,23 @@ namespace {
 
 	/** Utility function that can be called for each row to fill the map with values.
 		The map is deep #columns-1 and the last column contains leaf values. */
-	function hip_map_fill(&$map,&$row,$i){
-		if($i>=count($row)-2)
+	function hip_map_fill(&$map,&$row,$column,$i){
+		if($i>=count($column)-2)
 			// time to put the value in the last level
-			$map[$row[$i]] = $row[$i+1];
+			$map[$row[$column[$i]]] = $row[$column[$i+1]];
 		else
-			hip_map_fill($map[$row[$i]], $row,$i+1);
+			hip_map_fill($map[$row[$column[$i]]], $row,$column,$i+1);
 	}
 
 	/** Utility function that can be called for each row to fill the map with values. 
 		The map is deep #columns-1 and the last column contains leaf values.
 		Expects each leaf to possibly have more than one element for the same ekys, so leafs are arrays of final values. */
-	function hip_map_list_fill(&$map,&$row,$i){
-		if($i>=count($row)-2) 
+	function hip_map_list_fill(&$map,&$row,$column,$i){
+		if($i>=count($column)-2) 
 			// time to put the value in the array on the last level
-			$map[$row[$i]][] = $row[$i+1];
+			$map[$row[$column[$i]]][] = $row[$column[$i+1]];
 		else 
-			hip_map_list_fill($map[$row[$i]], $row,$i+1);
+			hip_map_list_fill($map[$row[$column[$i]]], $row,$column,$i+1);
 	}
 
 	/* Multi-level map fill utility function. Each leaf is a row  */
